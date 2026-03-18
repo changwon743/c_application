e4367ac01b251b1bad8cbc8d309716d6164f0d2e
one <multeen1472@gmail.com>
2026-03-18 11:37:25 +0900

    Implement functionality to compare and display integers

--- week06/08scoreswitch.c
+++ week06/08scoreswitch.c
@@ -1,2 +1,17 @@
 #include <stdio.h>
-int main(void) { return 0; }
+int main(void) {
+int a = 0, b = 0;
+printf("두 정수 입력 >>");
+scanf("%d%d", &a, &b);
+
+printf("최댓값: %d ", (a > b) ? a : b);
+printf("최솟값: %d\n", (a < b) ? a : b);
+printf("절대값: %d ", (a > 0) ? a : -a);
+printf("절대값: %d\n", (b > 0) ? b : -b);
+
+((a % 2) == 0 ) ? printf("짝수 ") : printf("홀수 ");
+printf("%s\n", ((b % 2) == 0) ? "짝수" : "홀수");
+
+return 0;
+
+}
