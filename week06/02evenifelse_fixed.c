4a73329b3cbb81246096f19c0e0c3b99dfd371db
one <multeen1472@gmail.com>
2026-03-18 10:59:45 +0900

    Implement even/odd check in main function

--- week06/02evenifelse_fixed.c
+++ week06/02evenifelse_fixed.c
@@ -1,2 +1,16 @@
 #include <stdio.h>
-int main(void) { return 0; }
+int main(void) { 
+    int n;
+    printf("정수 입력 : ");
+    scanf("%d", &n);
+
+    if(n%2) {
+        printf("홀수\n");
+    }
+    else {
+        printf("짝수\n");
+    }
+    (n%2)?printf("홀수\n"):printf("짝수\n");
+    
+    return 0;  
+}
